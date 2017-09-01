17b9fe9ab490dcd0889e4cd35d46b9bf51293ba9
mrApptastic <henrikbeske@gmail.com>
2017-09-01 13:21:29 +0200

    Added drawStar

--- captainCanvas.js
+++ captainCanvas.js
@@ -75,13 +75,39 @@ var captainCanvas = function(canvas, tools, settings) {
             cpt.ct[funky](x, y, w, 0,2*Math.PI);
             cpt.ct.fill();
             cpt.ct.stroke();
+			cpt.ct.closePath();
         }
 		else if (funky == "fillStyle") {
 		    cpt.ct.fillStyle = v;
 		}
 		else if (funky == "strokeStyle") {
 		    cpt.ct.strokeStyle = v;
-		}        		
+		}
+		this.drawStar = function (cx,cy,spikes,outerRadius,innerRadius) {
+			var rot=Math.PI/2*3;
+			var x= cx;
+			var y= cy;
+			var step=Math.PI/spikes;
+			cpt.ct.beginPath();
+			cpt.ct.moveTo(cx,cy-outerRadius)
+			for(i=0;i<spikes;i++){
+				x=cx+Math.cos(rot)*outerRadius;
+				y=cy+Math.sin(rot)*outerRadius;
+				cpt.ct.lineTo(x,y)
+				rot+=step
+				x=cx+Math.cos(rot)*innerRadius;
+				y=cy+Math.sin(rot)*innerRadius;
+				cpt.ct.lineTo(x,y)
+				rot+=step
+			}
+			cpt.ct.lineTo(cx,cy-outerRadius);
+			cpt.ct.closePath();
+			cpt.ct.lineWidth=5;
+			cpt.ct.strokeStyle='blue';
+			cpt.ct.stroke();
+			cpt.ct.fillStyle='skyblue';
+			cpt.ct.fill();
+		};
     };
 	cpt.ref = function () {
 		cpt.id.setAttribute("width",window.innerWidth);
