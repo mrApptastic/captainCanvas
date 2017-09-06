6fdf2a9781a25c84e5e5133b431a284fd7dcbd36
mrApptastic <henrikbeske@gmail.com>
2017-09-06 20:35:53 +0200

    Added more shapes

--- captainCanvas.js
+++ captainCanvas.js
@@ -8,7 +8,7 @@ var captainCanvas = function(canvas, tools, settings) {
 				"fit" : true,
 				"tog" : true
 			  };
-	cpt.fct = ["Firkant (Fyld)","Firkant (Streg)", "Firkant", "Cirkel", "Stjerne", "Trekant"];
+	cpt.fct = ["Firkant (Fyld)","Firkant (Streg)", "Firkant", "Cirkel", "Stjerne", "Trekant", "Trekant (Ret)", "Rhombe", "Trapezoid", "Ellipse"];
     cpt.col = ["AliceBlue", "AntiqueWhite", "Aqua", "Aquamarine", "Azure", "Beige", "Bisque", "Black", "BlanchedAlmond", "Blue", "BlueViolet", "Brown", "BurlyWood", "CadetBlue", "Chartreuse", "Chocolate", "Coral", "CornflowerBlue", "Cornsilk", "Crimson", "Cyan", "DarkBlue", "DarkCyan", "DarkGoldenRod", "DarkGray", "DarkGrey", "DarkGreen", "DarkKhaki", "DarkMagenta", "DarkOliveGreen", "DarkOrange", "DarkOrchid", "DarkRed", "DarkSalmon", "DarkSeaGreen", "DarkSlateBlue", "DarkSlateGray", "DarkSlateGrey", "DarkTurquoise", "DarkViolet", "DeepPink", "DeepSkyBlue", "DimGray", "DimGrey", "DodgerBlue", "FireBrick", "FloralWhite", "ForestGreen", "Fuchsia", "Gainsboro", "GhostWhite", "Gold", "GoldenRod", "Gray", "Grey", "Green", "GreenYellow", "HoneyDew", "HotPink", "IndianRed ", "Indigo ", "Ivory", "Khaki", "Lavender", "LavenderBlush", "LawnGreen", "LemonChiffon", "LightBlue", "LightCoral", "LightCyan", "LightGoldenRodYellow", "LightGray", "LightGrey", "LightGreen", "LightPink", "LightSalmon", "LightSeaGreen", "LightSkyBlue", "LightSlateGray", "LightSlateGrey", "LightSteelBlue", "LightYellow", "Lime", "LimeGreen", "Linen", "Magenta", "Maroon", "MediumAquaMarine", "MediumBlue", "MediumOrchid", "MediumPurple", "MediumSeaGreen", "MediumSlateBlue", "MediumSpringGreen", "MediumTurquoise", "MediumVioletRed", "MidnightBlue", "MintCream", "MistyRose", "Moccasin", "NavajoWhite", "Navy", "OldLace", "Olive", "OliveDrab", "Orange", "OrangeRed", "Orchid", "PaleGoldenRod", "PaleGreen", "PaleTurquoise", "PaleVioletRed", "PapayaWhip", "PeachPuff", "Peru", "Pink", "Plum", "PowderBlue", "Purple", "RebeccaPurple", "Red", "RosyBrown", "RoyalBlue", "SaddleBrown", "Salmon", "SandyBrown", "SeaGreen", "SeaShell", "Sienna", "Silver", "SkyBlue", "SlateBlue", "SlateGray", "SlateGrey", "Snow", "SpringGreen", "SteelBlue", "Tan", "Teal", "Thistle", "Tomato", "Transparent", "Turquoise", "Violet", "Wheat", "White", "WhiteSmoke", "Yellow", "YellowGreen"];
 	cpt.brs = {"Hgt" : 10, "Wth" : 10, "Fil" : "Black", "Str" : "Transparent" };
 	cpt.drg = false;
@@ -42,26 +42,26 @@ var captainCanvas = function(canvas, tools, settings) {
 		    z.push({"Fct" : "stroke" });				      
         }
 		else if (f == "Stjerne") {
-		    let thisx = x;
-			let thisy  = y;
-			let spikes = 5;
-			let innerRadius = w;
+            let innerRadius = w;
 			let outerRadius = h;
-			let rot = Math.PI / (2 * 3);
+            let spikes = 5;
+            let rot = Math.PI / 2 * 3;
+			let thisx = x;
+			let thisy = y;
 			let step = Math.PI / spikes;
 			z.push({"Fct" : "beginPath" });
-			z.push({"Fct" : "moveTo", "X" : x, "Y" : y - outerRadius});
+            z.push({"Fct" : "moveTo", "X" : x, "Y" : y - outerRadius});
 			for(let i = 0; i < spikes; i++){
-			    thisx = x + Math.cos(rot) * outerRadius;
+				thisx = x + Math.cos(rot) * outerRadius;
 				thisy = y + Math.sin(rot) * outerRadius;
 				z.push({"Fct" : "lineTo", "X" : thisx, "Y" : thisy});
-				rot += step;
-				thisx= x + Math.cos(rot) * innerRadius;
-				thisy = y + Math.sin(rot) * innerRadius;
+				rot += step
+				thisx=x + Math.cos(rot) * innerRadius;
+				thisy=y + Math.sin(rot) * innerRadius;
 				z.push({"Fct" : "lineTo", "X" : thisx, "Y" : thisy});
-				rot += step;
+				rot += step
 			}
-			z.push({"Fct" : "lineTo", "X" : x, "Y" : y - outerRadius});
+            z.push({"Fct" : "lineTo", "X" : x, "Y" : y - outerRadius});
 			z.push({"Fct" : "closePath" });
 			z.push({"Fct" : "fill" });
 			z.push({"Fct" : "stroke" });
@@ -75,14 +75,70 @@ var captainCanvas = function(canvas, tools, settings) {
 		    z.push({"Fct" : "fill" });
 		    z.push({"Fct" : "stroke" });	
 		}
+        else if (f == "Trekant (Ret)") {
+		    z.push({"Fct" : "beginPath" });
+            z.push({"Fct" : "moveTo", "X" : x, "Y" : y });
+            z.push({"Fct" : "lineTo", "X" : (x + w), "Y" : (y + h) }); 
+            z.push({"Fct" : "lineTo", "X" : x, "Y" : (y + h) });      
+		    z.push({"Fct" : "closePath" });	   
+		    z.push({"Fct" : "fill" });
+		    z.push({"Fct" : "stroke" });	
+        }
+        else if (f == "Rhombe") {
+  		    z.push({"Fct" : "beginPath" });
+            z.push({"Fct" : "moveTo", "X" : x, "Y" : y });
+            z.push({"Fct" : "lineTo", "X" : (x + w) - w/5, "Y" : y }); 
+            z.push({"Fct" : "lineTo", "X" : (x + w), "Y" : (y + h) });
+            z.push({"Fct" : "lineTo", "X" : x + w/5, "Y" : (y + h) });       
+		    z.push({"Fct" : "closePath" });	   
+		    z.push({"Fct" : "fill" });
+		    z.push({"Fct" : "stroke" });          
+        }
+        else if (f == "Trapezoid") {
+            
+        }
+        else if (f == "Ellipse") {
+            let kappa = .5522848;
+            let ox = (w / 2) * kappa;
+            let oy = (h / 2) * kappa;
+            let xe = x + w;
+            let ye = y + h;
+            let xm = x + w / 2;
+            let ym = y + h / 2; 
+            z.push({"Fct" : "beginPath" });
+            z.push({"Fct" : "moveTo", "X" : x, "Y" : y });
+            /*
+  ctx.bezierCurveTo(x, ym - oy, xm - ox, y, xm, y);
+  ctx.bezierCurveTo(xm + ox, y, xe, ym - oy, xe, ym);
+  ctx.bezierCurveTo(xe, ym + oy, xm + ox, ye, xm, ye);
+  ctx.bezierCurveTo(xm - ox, ye, x, ym + oy, x, ym);  
+  */    
+		    z.push({"Fct" : "closePath" });	   
+		    z.push({"Fct" : "fill" });
+		    z.push({"Fct" : "stroke" });    
+            /*
+              var kappa = .5522848,
+      ox = (w / 2) * kappa, // control point offset horizontal
+      oy = (h / 2) * kappa, // control point offset vertical
+      xe = x + w,           // x-end
+      ye = y + h,           // y-end
+      xm = x + w / 2,       // x-middle
+      ym = y + h / 2;       // y-middle
+
+  ctx.beginPath();
+  ctx.moveTo(x, ym);
+  ctx.bezierCurveTo(x, ym - oy, xm - ox, y, xm, y);
+  ctx.bezierCurveTo(xm + ox, y, xe, ym - oy, xe, ym);
+  ctx.bezierCurveTo(xe, ym + oy, xm + ox, ye, xm, ye);
+  ctx.bezierCurveTo(xm - ox, ye, x, ym + oy, x, ym);
+  ctx.closePath(); 
+  ctx.stroke();
+
+  */
+        }
 
         if (cpt.drg) {
             cpt.dt = cpt.dt.concat(z);
-            /*
-            for (let i = 0; i < z.length; i++) {                
-                cpt.dt.push(z[i]);
-            }
-            */
         }
 
 		cpt.dat();  
@@ -92,40 +148,6 @@ var captainCanvas = function(canvas, tools, settings) {
                 cpt.drafi(z[i].Fct, z[i].X, z[i].Y, z[i].Width, z[i].Height, z[i].Value, z[i].StartAngle, z[i].EndAngle);
             }
         }
-            
-/*
-			var rot=Math.PI/2*3;
-			var x= cx;
-			var y= cy;
-			var step=Math.PI/spikes;
-			cpt.ct.beginPath();
-			cpt.ct.moveTo(cx,cy-outerRadius)
-			for(i=0;i<spikes;i++){
-				x=cx+Math.cos(rot)*outerRadius;
-				y=cy+Math.sin(rot)*outerRadius;
-				cpt.ct.lineTo(x,y)
-				rot+=step
-				x=cx+Math.cos(rot)*innerRadius;
-				y=cy+Math.sin(rot)*innerRadius;
-				cpt.ct.lineTo(x,y)
-				rot+=step
-			}
-			cpt.ct.lineTo(cx,cy-outerRadius);
-			cpt.ct.closePath();
-			cpt.ct.lineWidth=5;
-			cpt.ct.strokeStyle='blue';
-			cpt.ct.stroke();
-			cpt.ct.fillStyle='skyblue';
-			cpt.ct.fill();
-				
-			
-          
-		}
-        else {
-            cpt.dat(); 
-            cpt.drafi(f, x, y, w, h, null, s, e);
-        }
-        */
 	};
 	cpt.cls = function () {
 		var fill = document.getElementsByClassName(cpt.tl.id + "_selectedFill")[0].value;
@@ -210,59 +232,6 @@ var captainCanvas = function(canvas, tools, settings) {
         else if (f == "transform" || f == "setTransform") {
             
         }
-			/*
-    	if (funky == "fillRect") {
-            cpt.ct[funky](x, y, w, h);
-		}
-		else if (funky == "strokeRect") {
-			cpt.ct[funky](x, y, w, h);
-		}
-        else if (funky == "rect") {
-            cpt.ct.beginPath();
-            cpt.ct[funky](x, y, w, h);
-            cpt.ct.fill();
-            cpt.ct.stroke();
-            cpt.ct.closePath();              
-        }
-        else if (funky == "arc") {
-            cpt.ct.beginPath();
-            cpt.ct[funky](x, y, w, s, e);
-            cpt.ct.fill();
-            cpt.ct.stroke();
-			cpt.ct.closePath();
-        }
-		else if (funky == "fillStyle") {
-		    cpt.ct.fillStyle = v;
-		}
-		else if (funky == "strokeStyle") {
-		    cpt.ct.strokeStyle = v;
-		}
-		this.drawStar = function (cx,cy,spikes,outerRadius,innerRadius) {
-			var rot=Math.PI/2*3;
-			var x= cx;
-			var y= cy;
-			var step=Math.PI/spikes;
-			cpt.ct.beginPath();
-			cpt.ct.moveTo(cx,cy-outerRadius)
-			for(i=0;i<spikes;i++){
-				x=cx+Math.cos(rot)*outerRadius;
-				y=cy+Math.sin(rot)*outerRadius;
-				cpt.ct.lineTo(x,y)
-				rot+=step
-				x=cx+Math.cos(rot)*innerRadius;
-				y=cy+Math.sin(rot)*innerRadius;
-				cpt.ct.lineTo(x,y)
-				rot+=step
-			}
-			cpt.ct.lineTo(cx,cy-outerRadius);
-			cpt.ct.closePath();
-			cpt.ct.lineWidth=5;
-			cpt.ct.strokeStyle='blue';
-			cpt.ct.stroke();
-			cpt.ct.fillStyle='skyblue';
-			cpt.ct.fill();
-		};
-		*/
     };
 	cpt.ref = function () {
 		cpt.id.setAttribute("width",window.innerWidth);
@@ -340,16 +309,6 @@ var captainCanvas = function(canvas, tools, settings) {
 							add += p[j] + ',';
 						}
 					}
-					/*
-					Fct -> fillStyle
-					Value -> Black
-					Fct -> strokeStyle
-					Value -> Transparent
-					if (j == "Value") {
-							html += name + "." + p.Fct + " = '" + p.Value + "';"
-					}
-					*/
-					//console.log(j + " -> " + p[j]);
 				}				
 			}
 			if (!isAtr) {
@@ -359,22 +318,10 @@ var captainCanvas = function(canvas, tools, settings) {
 				add += ');\r\n'
 			}
 			html += add;			
-		}
-		
-        /*
-        for (let i = 0; i < cpt.dt.length; i++) 
-            if (!cpt.dt[i].Value) {
-                html += name + "." + cpt.dt[i].Fct
-                for (j in cpt.dt[i]) {
-                    
-                }
-            }
-            else {
-                html += name + "." + cpt.dt[i].Fct + " = '" + cpt.dt[i].Value + "';"
-            }
-        } 
-        */
+		}		
+
         html += '</script>\r\n</body>\r\n</html>';
+
         cpt.down(name  + ".html", "data:text/html;charset=utf-8," + encodeURIComponent(html));
     };
 	cpt.wpng = function (fil) {
@@ -541,7 +488,6 @@ var captainCanvas = function(canvas, tools, settings) {
 			document.getElementById(cpt.tl.id + "_toolToggler").addEventListener("click", function(event) {
 				cpt.tl.style.display = !this.checked ?  "block" : "none";
 			});			
-			//<span id="toggleBandit"><span>Gem V&aelig;rkt&oslash;jer</span><input id="toolToggler" type="checkbox" /></span>
 		}
 		cpt.id.addEventListener("mousemove", function(event) {
             cpt.dim();
@@ -567,66 +513,3 @@ var captainCanvas = function(canvas, tools, settings) {
 	};
 	cpt.init();
 };
-	/*
-		this.id = document.getElementById(canvas);
-		this.context = this.id.getContext("2d");
-		this.properties = {"height" : 400, "width" : 400, "fixed" : false };
-		this.tools = document.getElementById(tools);
-		this.data = [];
-		this.brush = {"height" : 10, "width" : 10, "colours" : ["Red", "Green", "Blue","Orange","Yellow","Purple"]};
-		this.drawingFunctions = [{"Name" : "fillRect","Arguments" : ["X","Y","W","H"]},{"Name" : "strokeRect","Arguments" : ["X","Y","W","H"]}];
-		this.json = false;
-		this.drawing = false;
-		this.draw = function(event) {
-			if (this.drawing == true) {
-				var x = event.pageX - this.id.offsetLeft;
-				var y = event.pageY - this.id.offsetTop;
-				var w = this.brush.width;
-				var h = this.brush.height;
-				//this.context.fillRect(x,y,w,h);
-				this.data.push({"Fct" : document.getElementById(func).value, "X" : x, "Y" : y, "W" : w, "H" : h});
-				// this.tools.innerHTML = JSON.stringify(Cpt.data);
-				this.drawData();
-			}
-		};
-		this.drawData = function () {
-			for (var i = 0; i < this.data.length; i++) {
-				// eval("this.context." + this.data[i].Fct + "(this.data[i].X,this.data[i].Y,this.data[i].W,this.data[i].H)");
-				this.context.fillRect(this.data[i].X,this.data[i].Y,this.data[i].W,this.data[i].H);
-			}
-		};
-		this.toogleTools = function (value) {
-			this.tools.style.display = value != true ?  "block" : "none";
-		};
-		this.init = function() {
-			this.id.setAttribute("width",window.innerWidth);
-			this.id.setAttribute("height",window.innerHeight);
-			this.drawData();
-		};
-		this.toolsInit = function() {
-			for (var i = 0; i < this.drawingFunctions.length; i++) {
-				document.getElementById(func).innerHTML += "<option value='" + this.drawingFunctions[i].Name + "'>" + this.drawingFunctions[i].Name + "</option>";
-			}
-		};
-	};
-	til lyden af endnu en sang, hvis budskab kan stille sig i kø med lignende budskaber om fr hos ligeglade og ligegyldige mennesker.
-	
-	Prerequisites
-
-Following are the prerequisites for creating a VPN connection and for connecting to your organization network by using the VPN connection.
-
-    To create a VPN connection, you must know the IP address or fully qualified domain name (FQDN) of the VPN server to which you want to connect. For example, if you work for an organization named Contoso and the Contoso VPN server is named VPN1.contoso.com and has an IP address of 10.0.0.25, you must have this information ready when you create your VPN connection to the server.
-
-    To connect to a VPN server at your organization, you must know your user name and password. If you do not know your user name and password, you should talk to your organization Help Desk before attempting to connect to the organization network.
-
-    To connect to a VPN server at your organization, you must have permission from your organization to do so. If you are unable to connect to your organization network by using a VPN connection, you should talk to your organization Help Desk to find out if you have permission to connect from a remote location.
-
-    To connect to a VPN server at your organization, your computer or device must be connected to the Internet.
-
-    If your organization requires you to use a smart card to connect with VPN, you must have a smart card reader and a smart card connected to your computer or device before you attempt to connect to your organization. If your organization uses virtual smart cards, you must have a virtual smart card installed on the computer or device.
-
-    If you are unable to create a VPN connection by using this topic and if your computer or device is joined to your organization's domain, it is possible that your organization does not permit creation of VPN connections on your computer. If this is the case, you should talk to your organization Help Desk to find out if you have permission to create VPN connections to connect to your organization network.
-	
-	185.101.89.35
-	*/
-	
