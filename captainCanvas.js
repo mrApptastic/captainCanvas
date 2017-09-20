2018826f7e659b76ae63077e5aaca49c4d8234a3
mrApptastic <henrikbeske@gmail.com>
2017-09-20 23:18:13 +0200

    Added data edit area as simple json edit. Remapped default quick keys yet again. Made small changes of style.

--- captainCanvas.js
+++ captainCanvas.js
@@ -578,11 +578,37 @@ var captainCanvas = function(canvas, tools, settings) {
 				document.getElementsByClassName(cpt.tl.id + "_exportPNG")[0].addEventListener("click", function() {
 					cpt.wpng(document.getElementsByClassName(cpt.tl.id + "_fileName")[0].value);
 				});
+                cpt.tl.appendChild(document.createElement("br"));
 			/* Edit Data Toggler */
+                var dtt = document.createElement("span");
+                dtt.innerHTML = "Rediger Data : ";
+                cpt.tl.appendChild(dtt);
+                var dtg = document.createElement("input");
+                dtg.setAttribute("type", "checkbox");                
+                dtg.className = cpt.tl.id + "_dataToggler";
+                cpt.tl.appendChild(dtg);
+                document.getElementsByClassName(cpt.tl.id + "_dataToggler")[0].addEventListener("change", function(event) {
+                    if (this.checked) {
+                        document.getElementsByClassName(cpt.tl.id + "_dataEditArea")[0].value = JSON.stringify(cpt.dt);
+                        document.getElementsByClassName(cpt.tl.id + "_dataEditArea")[0].style.display = "block";
+                    }
+                    else {
+                        document.getElementsByClassName(cpt.tl.id + "_dataEditArea")[0].value = "";
+                        document.getElementsByClassName(cpt.tl.id + "_dataEditArea")[0].style.display = "none";
+                    }
+                });
+                cpt.tl.appendChild(document.createElement("br"));
 			/* Data Edit Area */
+                var dtf = document.createElement("textarea");
+                dtf.style.cssText = "height: 200px; overflow-y : auto; display: none;";
+                dtf.className = cpt.tl.id + "_dataEditArea";
+                cpt.tl.appendChild(dtf);
+                document.getElementsByClassName(cpt.tl.id + "_dataEditArea")[0].addEventListener("blur", function(event) {
+                    cpt.dt = JSON.parse(this.value);
+                });
 		/* Tool Box Toggler */
 		if (cpt.set.tog) {
-			cpt.tl.style.cssText = "position: fixed; right: 0; top: 0; z-index: 98; width: 30vw; overflow-y : auto;"
+			cpt.tl.style.cssText = "position: fixed; right: 0; top: 0; z-index: 98; width: 30vw;"; // overflow-y : auto;
 			let toggler = document.createElement("span");
 			let txt = document.createElement("span");
 			let chk = document.createElement("input");
@@ -713,12 +739,31 @@ var captainCanvas = function(canvas, tools, settings) {
 		});
 		cpt.id.addEventListener("mouseup", function() {
 			cpt.drg = false;
+            if (document.getElementsByClassName(cpt.tl.id + "_dataToggler")[0].checked) {
+                //document.getElementsByClassName(cpt.tl.id + "_dataEditArea")[0].innerHTML = "";
+                document.getElementsByClassName(cpt.tl.id + "_dataEditArea")[0].value = JSON.stringify(cpt.dt);
+                document.getElementsByClassName(cpt.tl.id + "_dataEditArea")[0].style.display = "block";
+                /*
+                for (let i = cpt.dt.length; i >= 0; i--) {
+                    let index = cpt.dt.length - i;
+                    let funky = cpt.dt[index].Fct.toString();
+                    document.getElementsByClassName(cpt.tl.id + "_dataEditArea")[0].innerHTML += i 
+                    + " " + '<input type="text" value="' + funky + '"/><br/>';
+                }
+                */
+            }
+            else {
+                document.getElementsByClassName(cpt.tl.id + "_dataEditArea")[0].value = "";
+                document.getElementsByClassName(cpt.tl.id + "_dataEditArea")[0].style.display = "none";
+            }
+            /*
 			if (document.getElementsByClassName(cpt.tl.id + "_selectedFunction")[0].value != "Linie") {	
 				
 			}
 			else {
 				
 			}
+            */
 		});
 		cpt.id.addEventListener("mouseout", function() {
 			cpt.drg = false;
@@ -730,18 +775,18 @@ var captainCanvas = function(canvas, tools, settings) {
             window.addEventListener("keydown", function (event) {
                 var k = event.keyCode;
                 switch (k) {
-                    case 49 : document.getElementsByClassName(cpt.tl.id + "_selectedFunction")[0].value = (parseInt(cpt.fct.indexOf(cpt.brs.Slc)) > 0 ? cpt.fct[parseInt(cpt.fct.indexOf(cpt.brs.Slc)) - 1] : cpt.fct[parseInt(cpt.fct.indexOf(cpt.brs.Slc))]); break;
-                    case 50 : document.getElementsByClassName(cpt.tl.id + "_selectedFunction")[0].value = (parseInt(cpt.fct.indexOf(cpt.brs.Slc)) < cpt.fct.length - 1 ? cpt.fct[parseInt(cpt.fct.indexOf(cpt.brs.Slc)) + 1] : cpt.fct[parseInt(cpt.fct.indexOf(cpt.brs.Slc))]); break;
-                    case 51 : document.getElementsByClassName(cpt.tl.id + "_height")[0].value = parseInt(document.getElementsByClassName(cpt.tl.id + "_height")[0].value) - 1; break;
-                    case 52 : document.getElementsByClassName(cpt.tl.id + "_height")[0].value = parseInt(document.getElementsByClassName(cpt.tl.id + "_height")[0].value) + 1; break;
-                    case 53 : document.getElementsByClassName(cpt.tl.id + "_width")[0].value = parseInt(document.getElementsByClassName(cpt.tl.id + "_width")[0].value) - 1; break;
-                    case 54 : document.getElementsByClassName(cpt.tl.id + "_width")[0].value = parseInt(document.getElementsByClassName(cpt.tl.id + "_width")[0].value) + 1; break;
-					case 55 : document.getElementsByClassName(cpt.tl.id + "_lineWidth")[0].value = parseInt(document.getElementsByClassName(cpt.tl.id + "_lineWidth")[0].value) > 0 ? parseInt(document.getElementsByClassName(cpt.tl.id + "_lineWidth")[0].value) - 1 : parseInt(document.getElementsByClassName(cpt.tl.id + "_lineWidth")[0].value); cpt.clw(); break;                    
-                    case 56 : document.getElementsByClassName(cpt.tl.id + "_lineWidth")[0].value = parseInt(document.getElementsByClassName(cpt.tl.id + "_lineWidth")[0].value) + 1; cpt.clw(); break;                    
-                    case 57 : document.getElementsByClassName(cpt.tl.id + "_selectedFill")[0].value = (parseInt(cpt.col.indexOf(cpt.brs.Fil)) > 0 ? cpt.col[parseInt(cpt.col.indexOf(cpt.brs.Fil)) - 1] : cpt.col[parseInt(cpt.col.indexOf(cpt.brs.Fil))]); cpt.cfl(); break;
-                    case 48 : document.getElementsByClassName(cpt.tl.id + "_selectedFill")[0].value = (parseInt(cpt.col.indexOf(cpt.brs.Fil)) < cpt.col.length - 1 ? cpt.col[parseInt(cpt.col.indexOf(cpt.brs.Fil)) + 1] : cpt.col[parseInt(cpt.col.indexOf(cpt.brs.Fil))]); cpt.cfl(); break;
-                    case 171 : document.getElementsByClassName(cpt.tl.id + "_selectedStroke")[0].value = (parseInt(cpt.col.indexOf(cpt.brs.Str)) > 0 ? cpt.col[parseInt(cpt.col.indexOf(cpt.brs.Str)) - 1] : cpt.col[parseInt(cpt.col.indexOf(cpt.brs.Str))]); cpt.cst(); break;
-                    case 192 : document.getElementsByClassName(cpt.tl.id + "_selectedStroke")[0].value = (parseInt(cpt.col.indexOf(cpt.brs.Str)) < cpt.col.length - 1 ? cpt.col[parseInt(cpt.col.indexOf(cpt.brs.Str)) + 1] : cpt.col[parseInt(cpt.col.indexOf(cpt.brs.Str))]); cpt.cst(); break;				
+                    case 81 : document.getElementsByClassName(cpt.tl.id + "_selectedFunction")[0].value = (parseInt(cpt.fct.indexOf(cpt.brs.Slc)) > 0 ? cpt.fct[parseInt(cpt.fct.indexOf(cpt.brs.Slc)) - 1] : cpt.fct[parseInt(cpt.fct.indexOf(cpt.brs.Slc))]); break;
+                    case 65 : document.getElementsByClassName(cpt.tl.id + "_selectedFunction")[0].value = (parseInt(cpt.fct.indexOf(cpt.brs.Slc)) < cpt.fct.length - 1 ? cpt.fct[parseInt(cpt.fct.indexOf(cpt.brs.Slc)) + 1] : cpt.fct[parseInt(cpt.fct.indexOf(cpt.brs.Slc))]); break;
+                    case 87 : document.getElementsByClassName(cpt.tl.id + "_height")[0].value = parseInt(document.getElementsByClassName(cpt.tl.id + "_height")[0].value) - 1; break;
+                    case 83 : document.getElementsByClassName(cpt.tl.id + "_height")[0].value = parseInt(document.getElementsByClassName(cpt.tl.id + "_height")[0].value) + 1; break;
+                    case 69 : document.getElementsByClassName(cpt.tl.id + "_width")[0].value = parseInt(document.getElementsByClassName(cpt.tl.id + "_width")[0].value) - 1; break;
+                    case 68 : document.getElementsByClassName(cpt.tl.id + "_width")[0].value = parseInt(document.getElementsByClassName(cpt.tl.id + "_width")[0].value) + 1; break;
+					case 82 : document.getElementsByClassName(cpt.tl.id + "_lineWidth")[0].value = parseInt(document.getElementsByClassName(cpt.tl.id + "_lineWidth")[0].value) > 0 ? parseInt(document.getElementsByClassName(cpt.tl.id + "_lineWidth")[0].value) - 1 : parseInt(document.getElementsByClassName(cpt.tl.id + "_lineWidth")[0].value); cpt.clw(); break;                    
+                    case 70 : document.getElementsByClassName(cpt.tl.id + "_lineWidth")[0].value = parseInt(document.getElementsByClassName(cpt.tl.id + "_lineWidth")[0].value) + 1; cpt.clw(); break;                    
+                    case 84 : document.getElementsByClassName(cpt.tl.id + "_selectedFill")[0].value = (parseInt(cpt.col.indexOf(cpt.brs.Fil)) > 0 ? cpt.col[parseInt(cpt.col.indexOf(cpt.brs.Fil)) - 1] : cpt.col[parseInt(cpt.col.indexOf(cpt.brs.Fil))]); cpt.cfl(); break;
+                    case 71 : document.getElementsByClassName(cpt.tl.id + "_selectedFill")[0].value = (parseInt(cpt.col.indexOf(cpt.brs.Fil)) < cpt.col.length - 1 ? cpt.col[parseInt(cpt.col.indexOf(cpt.brs.Fil)) + 1] : cpt.col[parseInt(cpt.col.indexOf(cpt.brs.Fil))]); cpt.cfl(); break;
+                    case 89 : document.getElementsByClassName(cpt.tl.id + "_selectedStroke")[0].value = (parseInt(cpt.col.indexOf(cpt.brs.Str)) > 0 ? cpt.col[parseInt(cpt.col.indexOf(cpt.brs.Str)) - 1] : cpt.col[parseInt(cpt.col.indexOf(cpt.brs.Str))]); cpt.cst(); break;
+                    case 72 : document.getElementsByClassName(cpt.tl.id + "_selectedStroke")[0].value = (parseInt(cpt.col.indexOf(cpt.brs.Str)) < cpt.col.length - 1 ? cpt.col[parseInt(cpt.col.indexOf(cpt.brs.Str)) + 1] : cpt.col[parseInt(cpt.col.indexOf(cpt.brs.Str))]); cpt.cst(); break;				
                 }
             })
         }
